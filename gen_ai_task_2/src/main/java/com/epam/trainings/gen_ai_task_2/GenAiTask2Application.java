2fe9c7f7174347d692c50a26a46e85109417d6c6
Pravallika Kothamasu <pravallika_kothamasu@epam.com>
2025-03-20 16:29:10 +0530

    Added task-2 for Gen AI trainings

---
+++ gen_ai_task_2/src/main/java/com/epam/trainings/gen_ai_task_2/GenAiTask2Application.java
@@ -0,0 +1,13 @@
+package com.epam.trainings.gen_ai_task_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenAiTask2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenAiTask2Application.class, args);
+	}
+
+}
